846e8178082cc45ab0b35e055e3c47b219d3aeea
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-11 00:48:23 +0200

    initiall commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,51 @@
-#include<iostream>
-int main()
-{
-	return 0;
+#include <iostream>
+
+#include <ctime>   
+
+using namespace std;
+
+
+template <typename T>
+void findMinMax(const T arr[], int size, T& minValue, int& minIndex, T& maxValue, int& maxIndex) {
+    minValue = arr[0];
+    minIndex = 0;
+    maxValue = arr[0];
+    maxIndex = 0;
+
+    for (int i = 1; i < size; ++i) {
+        if (arr[i] < minValue) {
+            minValue = arr[i];
+            minIndex = i;
+        }
+        if (arr[i] > maxValue) {
+            maxValue = arr[i];
+            maxIndex = i;
+        }
+    }
+}
+
+int main() {
+    const int size = 10;
+    int arr[size];
+
+    srand(static_cast<unsigned>(time(nullptr)));
+    for (int i = 0; i < size; ++i) {
+        arr[i] = rand() % 100; 
+    }
+
+    
+    cout << "Array: ";
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    int minValue, minIndex, maxValue, maxIndex;
+    findMinMax(arr, size, minValue, minIndex, maxValue, maxIndex);
+
+   
+    cout << "Min value: " << minValue << ", Index: " << minIndex << endl;
+    cout << "Max value: " << maxValue << ", Index: " << maxIndex << endl;
+
+    return 0;
 }
